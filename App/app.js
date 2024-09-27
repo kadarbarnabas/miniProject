7949f055fc0d5602a9a2668cf8901d6291780b2f
Kadar Barnabas <kadar.barnabas.16@gmail.com>
2024-09-27 12:28:14 +0200

    Updated app.js with new path to UI

--- App/app.js
+++ App/app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Statikus fájlok kiszolgálása (HTML, CSS)
-app.use(express.static(path.join(__dirname, 'UI')));
+// Statikus fájlok kiszolgálása a UI mappából
+app.use(express.static(path.join(__dirname, '..', 'UI')));
 
-// Egyszerű útvonal a Hello World-hoz
+// Gyökér útvonal (/) kiszolgálása
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'UI', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'UI', 'index.html'));
 });
 
 // Szerver indítása
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} (http://localhost:3000)`);
 });
